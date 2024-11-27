4f257d475b55c50124407615198e934060a8d6ce
Paul Stelzer <paul.stelzer@innomobile.de>
2024-11-27 18:03:53 +0100

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import { CustomDatePipe } from './custom-date.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatePipe, CustomDatePipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'coding-example';
-
-  get time() {
-    return new Date();
-  }
-
-  constructor(private cdr: ChangeDetectorRef) {
-    setInterval(() => {
-      this.updateTime();
-    }, 500)
-  }
-
-  updateTime() {
-    // update the time
-    this.cdr.detectChanges()
-  }
 }
